d90c3717d3e9291d7414ac47ae1d1aa3ad23dc93
AnshChaurasiya <anshchaurasiya239@gmail.com>
2026-06-11 19:19:44 +0530

    Stop tracking test files

--- smart-hospital/cypress/e2e/appointment/reschedule.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('Reschedule', () => {
-  beforeEach(() => cy.login('patient@test.com', 'Password1!'));
-
-  it('opens the reschedule flow for an existing appointment', () => {
-    cy.visit('/appointments');
-    cy.get('[data-cy="reschedule-appointment"]').first().click();
-    cy.get('[data-cy="reschedule-confirm"]').should('exist');
-  });
-});
